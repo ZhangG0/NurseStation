fc767811ae025a263e481ae0ee1f432543b2401d
ZhangG0 <zhanggzero@qq.com>
2022-12-08 18:03:19 +0800

    add: 新增持久化pinia

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+//注册pinia
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate"; //持久化
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 //引入elementPlus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 //引入iconfont
 import './assets/style/fonts/iconfont.css'
 //引入vant
-import { Button} from 'vant';
 import 'vant/es/toast/style';
 
 import App from './App.vue'
@@ -19,5 +22,5 @@ const app = createApp(App)
 app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
-app.use(Button)
+app.use(pinia)
 app.mount('#app')
