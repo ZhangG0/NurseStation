bab2464c7d81834eafee99a85e5f15163371c2a7
ZhangG0 <zhanggzero@qq.com>
2022-12-12 15:55:30 +0800

    update: 优化request

--- src/utils/Request.js
+++ src/utils/Request.js
@@ -1,13 +1,18 @@
 import axios from "axios";
+import {Toast} from "vant";
+import router from "@/router/index.js";
+import {useUserStore} from "@/stores/userStore.js";
 
-
+const userStore = useUserStore();
 const Request = axios.create({
     baseURL:'http://localhost:9090',
     timeout:5000
 })
 
 Request.interceptors.request.use(config => {
-    config.headers['Content-Type'] = 'application/json;charset=utf-8';
+    if (config.headers['Content-Type'] === "" || config.headers['Content-Type'] === null) {
+        config.headers['Content-Type'] = 'application/json;charset=utf-8';
+    }
     // 在浏览器的 Session Storage 中拿到 token 值
     config.headers.Authorization = window.localStorage.getItem("token");
     return config;
@@ -20,12 +25,21 @@ Request.interceptors.response.use(response => {
             return res
         }
         // 兼容服务端返回的字符串数据
-        // if (typeof res === 'string') {
-        //     res = res ? JSON.parse(res) : res
-        // }
+        if (typeof res === 'string') {
+            res = res ? JSON.parse(res) : res
+        }
+        if (res.status === -99){
+            sessionStorage.removeItem("user")
+            userStore.$reset();
+            router.replace('/community').then(() => Toast.fail("身份异常！请重新登录！！"))
+        }
         return res;
     },
     error => {
+        if (error.message === "timeout of 5000ms exceeded") {
+            error.message = "【请求超时5秒】" + error.message
+            Toast.fail("服务器连接超时请重试！")
+        }
         console.log('err' + error) // for debug
         return Promise.reject(error)
     }
