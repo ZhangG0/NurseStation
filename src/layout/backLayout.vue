c987d4de1fcd33baf2fe910bfc12497c721d0309
ZhangG0 <zhanggzero@qq.com>
2023-01-10 16:38:20 +0800

    update: 新增emit【setTitle】方法，可以设置标题

--- src/layout/backLayout.vue
+++ src/layout/backLayout.vue
@@ -4,15 +4,23 @@
       class="navBar"
       left-arrow
       @click-left="onClickLeft"
+      :title="title"
   />
   <div>
-    <router-view></router-view>
+    <router-view @setTitle="setTitle" ></router-view>
   </div>
 </div>
 </template>
 
 <script setup>
+import {ref} from 'vue'
+
 const onClickLeft = () => history.back();
+const title = ref("");
+const setTitle = (theTitle) => {
+  title.value = theTitle;
+  console.log(theTitle);
+}
 </script>
 
 <style lang="scss" scoped>
