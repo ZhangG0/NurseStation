a9283f22bda3a4086806ee213e6b95f0d95511f9
ZhangG0 <zhanggzero@qq.com>
2023-02-20 15:35:55 +0800

    add: 新增https

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,21 @@
-import {fileURLToPath, URL} from 'node:url'
-
+import path from 'path';
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
 import {VantResolver} from 'unplugin-vue-components/resolvers';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
         port: 8080,
         host: "0.0.0.0",
-        open: true
+        open: true,
+        https:true
     },
     plugins: [
         vue(),
+        basicSsl(),
         Components({
             resolvers: [VantResolver()],
         }),
@@ -29,7 +31,7 @@ export default defineConfig({
 
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+            '@': path.resolve(__dirname, './src'),
         }
     },
 
